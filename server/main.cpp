225deff69085c60e6da89c0d71408ab0e7ced52c
KangDroid <hyunwoo286@gmail.com>
2020-10-25 12:04:04 +0900

    Server: add some comment & vari-ize counter

--- server/main.cpp
+++ server/main.cpp
@@ -3,10 +3,19 @@
 
 using namespace std;
 
+// Maximum command to print/execute
+#define MAXIMUM_COUNT 10
+
+/**
+ * Server Program.
+ * If main function uses receive_exec(), it means it will execute every command sent from client.
+ * If main function uses receive_print(), it means it will just print out every command sent from client.
+ */
+
 int main(void) {
     Server server;
     int counter = 0;
-    while (counter < 10) {
+    while (counter < MAXIMUM_COUNT) {
         server.receive_exec();
         counter++;
     }
